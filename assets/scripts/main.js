158c51a831a66942af7de05258b960330220864f
vahid afzali <afzalivahid05@gmail.com>
2026-09-12 12:21:22 +0200

    Update theme-toggle color

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,10 +4,10 @@ const themeIcon = document.querySelector('#themeIcon');
 themeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains('dark-theme')) {
-        themeIcon.classList.remove('bi-moon-fill');
-        themeIcon.classList.add('bi-sun-fill');
-    } else {
         themeIcon.classList.remove('bi-sun-fill');
         themeIcon.classList.add('bi-moon-fill');
+    } else {
+        themeIcon.classList.remove('bi-moon-fill');
+        themeIcon.classList.add('bi-sun-fill');
     }   
 });
